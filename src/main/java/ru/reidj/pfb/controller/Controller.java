a407bea23beda1d38a060089e02be30a23737551
Reidj3 <sp1.ke@mail.ru>
2020-02-26 18:30:36 +0300

    Мелкие правки

--- src/main/java/ru/reidj/pfb/controller/Controller.java
+++ src/main/java/ru/reidj/pfb/controller/Controller.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -18,7 +19,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller {
-
     @FXML
     private ResourceBundle resources;
 
@@ -47,6 +47,8 @@ public class Controller {
     }
 
     private static final Group root = new Group();
+    private static final int height = 1080;
+    private static final int width = 1920;
 
     public static void prepair(){
         root.addEventFilter(KeyEvent.ANY, e -> {
@@ -67,8 +69,8 @@ public class Controller {
         for (int i = -100; i < 100; i++) {
             TextArea textAreaX = new TextArea("" + i);
             textAreaX.setFont(font);
-            textAreaX.setTranslateX(i * 100 + 600);
-            textAreaX.setTranslateY(300);
+            textAreaX.setTranslateX(i * 100 + width / 2);
+            textAreaX.setTranslateY(height / 2);
             textAreaX.setEditable(false);
             textAreaX.setMaxHeight(40);
             textAreaX.setMinHeight(40);
@@ -77,15 +79,15 @@ public class Controller {
 
             TextArea textAreaY = new TextArea("" + -i);
             textAreaY.setFont(font);
-            textAreaY.setTranslateX(600);
-            textAreaY.setTranslateY(i * 100 + 300);
+            textAreaY.setTranslateX(width / 2);
+            textAreaY.setTranslateY(i * 100 + height / 2);
             textAreaY.setEditable(false);
             textAreaY.setMinHeight(100);
             textAreaX.setMaxHeight(100);
             textAreaY.setMaxWidth(40);
             root.getChildren().addAll(textAreaX, textAreaY);
         }
-        root.getChildren().addAll(new Line(600, 600, 600, 0), new Line(0, 300, 1200, 300));
+        root.getChildren().addAll(new Line(width / 2, height, width / 2, 0), new Line(0, height / 2, width, height / 2));
     }
 
     @FXML
@@ -105,26 +107,35 @@ public class Controller {
                 boolean first = true;
                 double previousX = 0;
                 double previousY = 0;
+                double firstX, firstY, lastX, lastY;
                 for (double i = Double.parseDouble(startT.getText()) * 10; i < Double.parseDouble(endT.getText()) * 10; i++) {
                     shell.setVariable("t", i / 10);
                     if(first) {
-                        previousX = ((double) shell.evaluate(inputX.getText()) +6) * 100;
-                        previousY = - ((double) shell.evaluate(inputY.getText()) -3) * 100;
+                        previousX = ((double) shell.evaluate(inputX.getText())) * 100 + width / 2;
+                        previousY = - ((double) shell.evaluate(inputY.getText())) * 100 + height / 2;
                         first = false;
+                        firstX = previousX;
+                        firstY = previousY;
                         continue;
                     }
-                    double nowX = ((double) shell.evaluate(inputX.getText()) +6) * 100;
-                    double nowY =  - ((double) shell.evaluate(inputY.getText()) -3) * 100;
-                    root.getChildren().addAll(new Line(previousX, previousY, nowX, nowY));
+                    double nowX = ((double) shell.evaluate(inputX.getText())) * 100 + width / 2;
+                    double nowY =  - ((double) shell.evaluate(inputY.getText())) * 100 + height / 2;
+                    if(Double.parseDouble(endT.getText()) * 10 == i){
+                        lastX = nowX;
+                        lastY = nowY;
+                    }
+                    Line line = new Line(previousX, previousY, nowX, nowY);
+                    line.setStroke(Color.PINK);
+                    root.getChildren().add(line);
                     previousX = nowX;
                     previousY = nowY;
                 }
                 stage.setResizable(false);
-                stage.setHeight(600);
-                stage.setWidth(1200);
+                stage.setHeight(height);
+                stage.setWidth(width);
                 stage.setTitle("Проект Поддымова Сергея и Тенькова Алексея");
-                stage.setScene(new Scene(root));
-                stage.showAndWait();
+                stage.setScene(new Scene(root, Color.AZURE));
+                stage.show();
             });
         });
     }
